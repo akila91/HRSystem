3620b37a47e35ae5828bc47f4c01f7d8cd01f518
Akila <law@tiqri.com>
2023-06-30 12:25:43 +0800

    Initial commit

---
+++ Data/HRContext.cs
@@ -0,0 +1,24 @@
+﻿using HRSystemEmp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRSystemEmp.Data
+{
+    public class HRContext : DbContext
+    {
+        public HRContext(DbContextOptions<HRContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Department> Department { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+            modelBuilder.Entity<Department>().ToTable("Department");
+        }
+    }
+}
---
+++ Models/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRSystemEmp.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string DepartmentName { get; set; }
+        public string EmployeeEmail { get; set; }
+    }
+}
